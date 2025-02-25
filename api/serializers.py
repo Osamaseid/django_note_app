e2154954e56133bb38fa8fa86159529076f3a24a
osama seid <osaseid1@gmail.com>
2025-02-25 23:23:47 +0300

    views updated

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
-from rest_framework import serializers 
+from rest_framework import serializers
+from .models import Note 
 
 class userSerializer(serializers.ModelSerializer):
     class Meta:

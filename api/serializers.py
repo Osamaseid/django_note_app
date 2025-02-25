d760096b1002799314500cb4d7610caa8e2f1891
osama seid <osaseid1@gmail.com>
2025-02-25 22:23:37 +0300

    serialier added

--- api/serializers.py
+++ api/serializers.py
@@ -4,8 +4,8 @@ from rest_framework import serializers
 class userSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
-        etra_kwargs = {'password': {'write_only': True}}
+        fields = ['id', 'username', 'email', 'password']
+        etra_kwargs = {'password': {'write_only':True}}
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
